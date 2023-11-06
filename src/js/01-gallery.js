c880633775e03cba5f6be37e51dc645a6bc63c21
Ariy83 <143614766+Ariy83@users.noreply.github.com>
2023-11-07 01:16:33 +0200

    'update'

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListEl = document.querySelector('ul.gallery');
+
+function makeGallery(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+  });
+}
+
+function renderGallery(galleryItems) {
+  const markup = makeGallery(galleryItems).join('');
+  galleryListEl.insertAdjacentHTML('afterbegin', markup);
+}
+renderGallery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
 
+const iframeEl = document.querySelector('#vimeo-player');
+
+const player = new Player(iframeEl, {
+  id: 19231868,
+  width: 640,
+});
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+const onPlay = function ({ seconds }) {
+  console.log(seconds);
+};
+
+player.on('timeupdate', onPlay);
